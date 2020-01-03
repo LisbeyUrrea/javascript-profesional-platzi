284014fc48cfdb5fac6e7b178868a7015864c09e
Lisbey Urrea <lisbey.urrea@sofka.com.co>
2020-01-03 16:59:29 -0500

    practica javascript

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+
+const buttonPlay = document.querySelector('#play');
+buttonPlay.onclick = () => player.togglePlay();
+
+const buttonMute = document.querySelector('#mute');
+buttonMute.onclick = () => player.toggleMute();
